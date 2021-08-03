b5f320419edf841ba744d166f40696996ab25e62
Yury Zimnitski <demovie.inc@gmail.com>
2021-08-03 11:12:17 +0300

    Add usePathname hook

--- src/modules/sidebar/components/Menu/menu.jsx
+++ src/modules/sidebar/components/Menu/menu.jsx
@@ -1,27 +1,70 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import usePathname from 'modules/sidebar/hooks/usePathname';
 
 import './menu.css';
 
 
 function Menu() {
+  const { isActive } = usePathname();
+
   return (
     <menu className={classNames('menu')}>
       <Link
         className={classNames(
           'menu__link',
-          'menu__link_active',
+          { menu__link_active: isActive('/dashboard') },
         )}
         to="/dashboard"
       >
         Главная
       </Link>
-      <Link className={classNames('menu__link')} to="/calendar">Календарь</Link>
-      <Link className={classNames('menu__link')} to="/patients">Пациенты</Link>
-      <Link className={classNames('menu__link')} to="/report">Отчет</Link>
-      <Link className={classNames('menu__link')} to="/narcology">Наркология</Link>
-      <Link className={classNames('menu__link')} to="/psychiatry">Психиатрия</Link>
+      <Link
+        className={classNames(
+          'menu__link',
+          { menu__link_active: isActive('/calendar') },
+        )}
+        to="/calendar"
+      >
+        Календарь
+      </Link>
+      <Link
+        className={classNames(
+          'menu__link',
+          { menu__link_active: isActive('/patients') },
+        )}
+        to="/patients"
+      >
+        Пациенты
+      </Link>
+      <Link
+        className={classNames(
+          'menu__link',
+          { menu__link_active: isActive('/report') },
+        )}
+        to="/report"
+      >
+        Отчет
+      </Link>
+      <Link
+        className={classNames(
+          'menu__link',
+          { menu__link_active: isActive('/narcology') },
+        )}
+        to="/narcology"
+      >
+        Наркология
+      </Link>
+      <Link
+        className={classNames(
+          'menu__link',
+          { menu__link_active: isActive('/psychiatry') },
+        )}
+        to="/psychiatry"
+      >
+        Психиатрия
+      </Link>
     </menu>
   );
 }
